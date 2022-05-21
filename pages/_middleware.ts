59173fcca687abc6dd34aa3b21fed97450cc9de2
Trung Vu <trungdvu.it@gmail.com>
2022-05-22 00:38:53 +0700

    Add edge function

---
+++ pages/_middleware.ts
@@ -0,0 +1,14 @@
+// Not in Node env
+import { NextResponse } from "next/server";
+
+const signedinPages = ["/", "/playlist", "/library"];
+
+export default function middleware(req) {
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.SPOTIFY_ACCESS_TOKEN;
+
+    if (!token) {
+      return NextResponse.redirect("/signin");
+    }
+  }
+}
